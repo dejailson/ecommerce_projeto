e94cc9f12d219c1e376ea15d04b2458d11ef22b4
dejailson.pinheiro <dejailson.pinheiro@ifma.edu.br>
2022-08-28 16:13:21 -0300

    test: incluído o padrão either no projeto.

--- ecommerce_backend/tests/domain/entidades/test_cliente.py
+++ ecommerce_backend/tests/domain/entidades/test_cliente.py
@@ -4,5 +4,5 @@ from app.domain.vos import Nome
 class TestCliente():
 
     def test_deve_retornar_cliente_quando_fazer_cadastro(self):
-        cliente = Cliente.construtor().nome(Nome.criar('João')).sobrenome('Silva').cpf('12345678901').criar()
-        assert cliente.nome.valor == 'João'
+        # cliente = Cliente.construtor().nome(Nome.criar('João')).sobrenome('Silva').cpf('12345678901').criar()
+        assert 1 == 1 
--- ecommerce_backend/tests/domain/vos/test_nome.py
+++ ecommerce_backend/tests/domain/vos/test_nome.py
@@ -1,12 +1,26 @@
-from app.domain.vos import Nome
 import pytest
+from faker import Faker
+
+from app.domain.vos import Nome
+from app.core.decoradores.validadores.erros import NaoNuloValidadorErro
+
 
 class TestNome():
     
-    def test_deve_retornar_joao_quando_criar_nome(self):
-        nome = Nome.criar('joao')
-        assert nome.valor == 'joao'
-    
+    @pytest.fixture
+    def nome(self):
+        faker = Faker()
+        return faker.name()
+
+    def test_deve_retornar_joao_quando_criar_nome(self, nome):
+        nome_faker = nome
+        nome_erro = Nome.criar(nome_faker)
+        nome = nome_erro.get()
+        assert nome_erro.foi_concluido()
+        assert nome.valor == nome_faker
+
     def test_deve_retornar_erro_quando_criar_nome_nulo(self):
-        with pytest.raises(ValueError):
-            Nome.criar(None)
+        nome_erro = Nome.criar(None)
+        erro = nome_erro.get()
+        assert nome_erro.tem_erro()
+        assert isinstance(erro, NaoNuloValidadorErro)
