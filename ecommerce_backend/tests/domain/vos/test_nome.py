59e3ca939643e8046856825aba53564704a3f11a
dejailson <dejailson@gmail.com>
2022-09-09 08:42:26 -0300

    test: implementado o validador nao_branco_validador.

--- ecommerce_backend/tests/domain/vos/test_nome.py
+++ ecommerce_backend/tests/domain/vos/test_nome.py
@@ -3,6 +3,7 @@ from faker import Faker
 
 from app.domain.vos import Nome
 from app.core.decoradores.validadores.erros import NaoNuloValidadorErro
+from app.core.decoradores.validadores.erros import NaoBrancoValidadorErro
 
 
 class TestNome():
@@ -12,7 +13,7 @@ class TestNome():
         faker = Faker()
         return faker.name()
 
-    def test_deve_retornar_joao_quando_criar_nome(self, nome):
+    def test_deve_retornar_sucesso_quando_criar_nome(self, nome):
         nome_faker = nome
         nome_erro = Nome.criar(nome_faker)
         nome = nome_erro.get()
@@ -22,5 +23,14 @@ class TestNome():
     def test_deve_retornar_erro_quando_criar_nome_nulo(self):
         nome_erro = Nome.criar(None)
         erro = nome_erro.get()
+        mensagem = 'não pode ser nulo'
         assert nome_erro.tem_erro()
+        assert mensagem in erro.mensagem
         assert isinstance(erro, NaoNuloValidadorErro)
+    
+    def test_deve_retornar_nao_branco_validador_erro_quando_criar_nome_em_branco(self):
+        nome_branco = "  "
+        nome_erro = Nome.criar(nome_branco)
+        erro = nome_erro.get()
+        assert nome_erro.tem_erro()
+        assert isinstance(erro,NaoBrancoValidadorErro)
