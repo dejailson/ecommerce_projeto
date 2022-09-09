1476c6673e65a5f6d9894f90c4c75d3ea21d0389
dejailson <dejailson@gmail.com>
2022-09-09 08:34:12 -0300

    feat: implementado o validador não_branco_validador

--- ecommerce_backend/app/core/decoradores/validadores/erros/__init__.py
+++ ecommerce_backend/app/core/decoradores/validadores/erros/__init__.py
@@ -1,3 +1,4 @@
 from .nao_nulo_validador_erro import NaoNuloValidadorErro
+from .nao_branco_validador_erro import NaoBrancoValidadorErro
 
-__all__ = ['NaoNuloValidadorErro']
+__all__ = ['NaoNuloValidadorErro', 'NaoBrancoValidadorErro']
--- ecommerce_backend/app/core/decoradores/validadores/validador.py
+++ ecommerce_backend/app/core/decoradores/validadores/validador.py
@@ -1,7 +1,8 @@
 from functools import wraps
 from typing import Any
 from ...erro import Either
-from .erros.nao_nulo_validador_erro import NaoNuloValidadorErro
+from .erros import NaoNuloValidadorErro
+from .erros import NaoBrancoValidadorErro
 
 class Validador():
       
@@ -12,6 +13,18 @@ class Validador():
             def wrapper(valor) -> Either[NaoNuloValidadorErro,Any]:
                 if valor is None:
                     return Either.erro(NaoNuloValidadorErro(mensagem))
+                return function(valor)
+            return wrapper
+        return decorator
+    
+    @classmethod
+    def nao_branco_validador(cls,mensagem):
+        def decorator(function):
+            @wraps(function)
+            @Validador.nao_nulo_validador(mensagem = 'Atributo não pode ser nulo.')
+            def wrapper(valor) -> Either[NaoBrancoValidadorErro,Any]:
+                if len(valor.strip()) == 0:
+                    return Either.erro(NaoBrancoValidadorErro(mensagem))
                 return function(valor)
             return wrapper
         return decorator
