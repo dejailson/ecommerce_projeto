f40fc287ba196155c76fef205a1476ec669f60c8
dejailson <dejailson@gmail.com>
2022-09-09 08:40:35 -0300

    feat: alterado o validador para nao_branco_validador.

--- ecommerce_backend/app/domain/vos/nome.py
+++ ecommerce_backend/app/domain/vos/nome.py
@@ -1,21 +1,21 @@
 from dataclasses import dataclass, field
 from typing import Union
-from app.core.decoradores.validadores import Validador
+from app.core.decoradores.validadores import Validador, Validador
 from app.core.erro import Either
-from app.core.decoradores.validadores.erros import NaoNuloValidadorErro
+from app.core.decoradores.validadores.erros import NaoBrancoValidadorErro
 
 
 @dataclass(frozen=True, eq=True, order=True)
 class Nome():
     __valor: str= field(default=None,init=True)
-    erros = Union[NaoNuloValidadorErro,None]
+    erros = Union[NaoNuloValidadorErro,NaoBrancoValidadorErro]
 
     @property
     def valor(self,) -> str:
         return self.__valor
 
     @staticmethod
-    @Validador.nao_nulo_validador(mensagem = 'Nome não pode ser nulo.')
+    @Validador.nao_branco_validador(mensagem = 'Nome deve conter um conjunto de caracteres.')
     def criar(nome: str = None) -> Either[erros,'Nome']:
         return Either.sucesso(Nome(nome))
 
